63adbdad06fa74534a11cc1e3d171e689af997a0
Jason Orendorff <jason.orendorff@gmail.com>
2016-07-26 17:11:49 -0500

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# indexor - A toy search engine!
+
+Nothing is working yet. But we do have a collection of files we want to work with:
+
+1.  Download https://www.dropbox.com/s/lv44vyl8ia46llx/sample.tar.bz2?dl=0
+    and unzip it. (It creates a directory named `sample` with about 8,476
+    files in it.)
+
+2.  Run `./build_index.py` to try to build the index.
+    Like we said, it doesn't work yet.
+
+
+
---
+++ build_index.py
@@ -0,0 +1,24 @@
+#!/bin/env python3
+
+import os
+import collections
+import shelve
+
+DIR = "./sample"
+
+with shelve.open("search-index") as index:
+    for filename in sorted(os.listdir(DIR)):
+        print(filename)
+        with open(os.path.join(DIR, filename)) as f:
+            tokens = f.read().lower().split()
+
+            # Now store every token
+            for token in tokens:
+                if token in index:
+                    temp = index[token]  # big O(n) read
+                else:
+                    temp = []
+                temp.append(filename)
+                index[token] = temp  # big O(n) write
+
+ 
