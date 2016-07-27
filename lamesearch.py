a92b9778b840111b390a32cc85c5bfe3519500bf
Jason Orendorff <jason.orendorff@gmail.com>
2016-07-27 16:22:08 -0500

    lamesearch

---
+++ lamesearch.py
@@ -0,0 +1,39 @@
+#!/bin/env python3
+
+import os, collections
+
+DIR = "./sample"
+filenames = sorted(os.listdir(DIR))
+del filenames[250:]
+titles = []
+
+index_by_term = collections.defaultdict(set)
+
+for i, filename in enumerate(filenames):
+    print(filename)
+    with open(os.path.join(DIR, filename)) as f:
+        title = f.readline().strip()
+        titles.append(title)
+        tokens = f.read().lower().split()
+
+    for term in tokens:
+        index_by_term[term].add(i)
+
+print("Loaded {} documents.".format(len(filenames)))
+print("There are {} distinct terms in the corpus.".format(len(index_by_term)))
+
+def handle_query(query):
+    results = index_by_term[query]
+    if results:
+        for doc_index in list(results)[:10]:
+            print("{:20s} {}".format(titles[doc_index], filenames[doc_index]))
+    else:
+        print("no hits")
+
+while True:
+    try:
+        query = input("> ").strip()
+    except EOFError as exc:
+        break
+    if query != "":
+        handle_query(query)
